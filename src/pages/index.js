7734261caba443257d7159e13f89ee970aec4335
Jatin Lodhi <jatinlodhi111@gmail.com>
2023-03-26 17:11:52 +0530

    removed varients file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { motion } from "framer-motion"
 import {BsMoonStarsFill } from "react-icons/bs" 
-import { fadeIn } from './varients'; 
+// import { fadeIn } from './varients'; 
 import {
   AiFillTwitterCircle,
   AiFillLinkedin,
@@ -31,7 +31,29 @@ import  {Link } from 'react-scroll';
 import {MdEmail} from 'react-icons/md' 
 import {SiLeetcode} from 'react-icons/si'
 import {BsGithub} from 'react-icons/bs'
- 
+
+export const fadeIn = (direction, delay) => {
+  return {
+    hidden: {
+      y: direction === 'up' ? 80 : direction === 'down' ? -80 : 0,
+      opacity: 0,
+      x: direction === 'left' ? 80 : direction === 'right' ? -80 : 0,
+    },
+    show: {
+      y: 0,
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: 'tween',
+        duration: 1.2,
+        delay: delay,
+        ease: [0.25, 0.25, 0.25, 0.75],
+      },
+    },
+  };
+};
+
+
 export default function Home() {
   const [darkMode,setDarkMode]  = useState(false);
    
